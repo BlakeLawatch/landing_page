f3338c755849ecc64d5520cd86f4662a8abf04b1
Blake Lawatch <blakelawatch@gmail.com>
2023-12-19 21:38:06 -0700

    landing page started

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <header>
+    <!-- <Navbar /> -->
+  </header>
+  <main class="main">
+    <router-view />
+  </main>
+  <footer>
+
+  </footer>
+</template>
+
+<script>
+import { computed } from 'vue'
+import { AppState } from './AppState'
+import Navbar from './components/Navbar.vue'
+
+export default {
+  setup() {
+    return {
+      appState: computed(() => AppState)
+    }
+  },
+  components: { Navbar }
+}
+</script>
+<style lang="scss">
+@import "./assets/scss/main.scss";
+
+:root {
+  --main-height: calc(100vh - 32px - 64px);
+}
+
+.main {
+  background-image: url('https://img.freepik.com/premium-photo/grunge-background-with-space-text_196038-5926.jpg?size=626&ext=jpg&ga=GA1.1.66302407.1699569051&semt=ais');
+}
+</style>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    hello world
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss"></style>
